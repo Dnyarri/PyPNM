6599c26b952896b92eb8d70e6c1e1938dff3cfd6
Ilya Razmanov <ilyarazmanov@gmail.com>
2025-02-01 09:31:15 +0300

    Image list turned to global. GUI improvements.

--- viewer.py
+++ viewer.py
@@ -2,7 +2,7 @@
 
 """Test shell for pnmlpnm module - Tkinter-based viewer.
 
-Viewer does not use PPM file directly to display it with Tkinter Tkinter PhotoImage(file=...) - it loads file (in this case - PPM or PGM, just because it's a demo for pnmlpnm module anyway), then constructs PPM-like bytes data object in memory, and then displays it using Tkinter PhotoImage(data=...). For example, it's able to display ascii PGM and PPM, not directly supported by Tkinter, since it recodes them to binary on the fly.
+Viewer does not use PPM file directly to display it with Tkinter Tkinter PhotoImage(file=...) - instead, it loads file (in this case - PPM or PGM, just because it's a demo for pnmlpnm module anyway), then constructs PPM-like bytes data object in memory, and then displays it using Tkinter PhotoImage(data=...). For example, it's able to display ascii PGM and PPM, not directly supported by Tkinter, since it recodes them to binary on the fly.
 
 """
 
@@ -10,7 +10,7 @@ __author__ = 'Ilya Razmanov'
 __copyright__ = '(c) 2024-2025 Ilya Razmanov'
 __credits__ = 'Ilya Razmanov'
 __license__ = 'unlicense'
-__version__ = '1.13.6.0'
+__version__ = '1.14.1.10'
 __maintainer__ = 'Ilya Razmanov'
 __email__ = 'ilyarazmanov@gmail.com'
 __status__ = 'Production'
@@ -30,6 +30,7 @@ def GetSource():
     """Opening source image and redefining other controls state"""
 
     global zoom_factor, sourcefilename, preview, preview_data
+    global X, Y, Z, maxcolors, image3D
     zoom_factor = 1
     sourcefilename = filedialog.askopenfilename(title='Open PPM/PGM file to view', filetypes=[('Portable map formats', '.ppm .pgm .pbm')])
     if sourcefilename == '':
@@ -37,9 +38,9 @@ def GetSource():
 
     """ ┌───────────────────────────────────────┐
         │ Loading file, converting data to list │
-        └──────────────────────────────────────-┘ """
+        │ NOTE: maxcolors, image3D are GLOBALS! │
+        └───────────────────────────────────────┘ """
     X, Y, Z, maxcolors, image3D = pnmlpnm.pnm2list(sourcefilename)
-
 
     label_info.config(text=f'X={X} Y={Y} Z={Z} maxcolors={maxcolors}')
     sortir.update()
@@ -66,12 +67,7 @@ def GetSource():
 
 
 def SaveAsBin():
-    """Once pressed on Save as bin"""
-
-    """ ┌───────────────────────────────────────┐
-        │ Loading file, converting data to list │
-        └──────────────────────────────────────-┘ """
-    X, Y, Z, maxcolors, image3D = pnmlpnm.pnm2list(sourcefilename)
+    """Once pressed on Save binary"""
 
     # Adjusting "Save to" formats to be displayed according to bitdepth
     if Z < 3:
@@ -99,12 +95,7 @@ def SaveAsBin():
 
 
 def SaveAsAscii():
-    """Once pressed on Save as ASCII"""
-
-    """ ┌───────────────────────────────────────┐
-        │ Loading file, converting data to list │
-        └──────────────────────────────────────-┘ """
-    X, Y, Z, maxcolors, image3D = pnmlpnm.pnm2list(sourcefilename)
+    """Once pressed on Save ASCII"""
 
     # Adjusting "Save to" formats to be displayed according to bitdepth
     if Z < 3:
@@ -179,7 +170,7 @@ sortir.minsize(300, 110)
 icon = PhotoImage(data=b'P6\n2 2\n255\n\xff\x00\x00\xff\xff\x00\x00\x00\xff\x00\xff\x00')
 sortir.iconphoto(True, icon)
 
-label_info = Label(sortir, text='PNM image file viewer', font=('courier', 8),  foreground='grey')
+label_info = Label(sortir, text='PNM image file viewer', font=('courier', 8), foreground='grey')
 label_info.pack(side='bottom', padx=0, pady=1, fill='both')
 
 frame_left = Frame(sortir, borderwidth=2, relief='groove')
@@ -187,19 +178,19 @@ frame_left.pack(side='left', anchor='nw')
 frame_right = Frame(sortir, borderwidth=2, relief='groove')
 frame_right.pack(side='left', anchor='nw')
 
-butt01 = Button(frame_left, text='Open PPM/PGM...', font=('arial', 16), cursor='hand2', justify='center', command=GetSource)
+butt01 = Button(frame_left, text='Open PPM/PGM...'.center(24, ' '), font=('helvetica', 16), cursor='hand2', justify='center', command=GetSource)
 butt01.pack(side='top', padx=4, pady=[2, 12], fill='both')
 
-butt02 = Button(frame_left, text='Save binary PPM/PGM...', font=('arial', 12), cursor='arrow', justify='center', state='disabled', command=SaveAsBin)
+butt02 = Button(frame_left, text='Save binary PPM/PGM...', font=('helvetica', 12), cursor='arrow', justify='center', state='disabled', command=SaveAsBin)
 butt02.pack(side='top', padx=4, pady=2, fill='both')
 
-butt03 = Button(frame_left, text='Save ASCII PPM/PGM...', font=('arial', 12), cursor='arrow', justify='center', state='disabled', command=SaveAsAscii)
+butt03 = Button(frame_left, text='Save ASCII PPM/PGM...', font=('helvetica', 12), cursor='arrow', justify='center', state='disabled', command=SaveAsAscii)
 butt03.pack(side='top', padx=4, pady=2, fill='both')
 
-butt99 = Button(frame_left, text='Exit', font=('arial', 16), cursor='hand2', justify='center', command=DisMiss)
+butt99 = Button(frame_left, text='Exit', font=('helvetica', 16), cursor='hand2', justify='center', command=DisMiss)
 butt99.pack(side='bottom', padx=4, pady=[12, 2], fill='both')
 
-zanyato = Label(frame_right, text='Preview area', font=('arial', 10), justify='center', borderwidth=2, relief='groove')
+zanyato = Label(frame_right, text='Preview area', font=('helvetica', 10), justify='center', borderwidth=2, relief='groove')
 zanyato.pack(side='top')
 
 frame_zoom = Frame(frame_right, width=300, borderwidth=2, relief='groove')
