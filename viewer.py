d55194cb46b628a543fc589d18422a038b037267
Ilya Razmanov <ilyarazmanov@gmail.com>
2025-04-15 18:21:29 +0300

    Viewer 2

--- viewer.py
+++ viewer.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
 
-"""Test shell for `pnmlpnm <https://github.com/Dnyarri/PyPNM/>`_ module - Tkinter-based viewer.
+"""Test shell for `PyPNM<https://github.com/Dnyarri/PyPNM/>`_ module - Tkinter-based viewer.
 
 NOTE: This is special Python 3.4 compatible version!
 
 Viewer does not use PPM file directly to display it with Tkinter PhotoImage(file=...) - 
-instead, it loads file (in this case - PPM, PGM, or PBM, just because it's a demo for pnmlpnm module anyway),
-then constructs PPM-like bytes data object in memory, and then displays it using Tkinter PhotoImage(data=...).
+instead, it loads image file, then constructs PPM-like bytes data object in memory,
+and then displays it using Tkinter PhotoImage(data=...).
 For example, it's able to display ascii PGM and PPM, not directly supported by Tkinter,
 since it recodes them to binary on the fly.
 
 """
 
 __author__ = 'Ilya Razmanov'
-__copyright__ = '(c) 2024-2025 Ilya Razmanov'
+__copyright__ = '(c) 2025 Ilya Razmanov'
 __credits__ = 'Ilya Razmanov'
 __license__ = 'unlicense'
-__version__ = '1.16.5.34'
+__version__ = '2.16.15.34'
 __maintainer__ = 'Ilya Razmanov'
 __email__ = 'ilyarazmanov@gmail.com'
 __status__ = 'Production'
 
 from pathlib import Path
-from tkinter import Button, Frame, Label, PhotoImage, Tk, filedialog
+from tkinter import Button, Frame, Label, Menu, PhotoImage, Tk, filedialog
+from tkinter.messagebox import showinfo
 
 from pypng import pnglpng
 from pypnm import pnmlpnm
@@ -30,19 +31,50 @@ from pypnm import pnmlpnm
 
 def DisMiss():
     """Kill dialog and continue"""
-
     sortir.destroy()
+
+
+def UINormal():
+    """Normal UI state"""
+    zanyato.config(state='normal', cursor='arrow')
+    sortir.update()
+
+
+def UIBusy():
+    """Busy UI state"""
+    zanyato.config(state='disabled', cursor='wait')
+    sortir.after(1, sortir.update())  # Otherwise cursor never updates
+    sortir.update()
+
+
+def ShowMenu(event):
+    """Pop menu up (or sort of drop it down)"""
+    menu01.post(event.x_root, event.y_root)
+
+
+def ShowInfo():
+    """Show program and module version"""
+    message = ''.join(('PNMViewer ver. ', str(__version__), '\nModules:\n', str(pnmlpnm.__name__), ' ver. ', str(pnmlpnm.__version__), '\n', str(pnglpng.__name__), ' ver. ', str(pnglpng.__version__), '\n', str(pnglpng.png.__name__), ' ver. ', str(pnglpng.png.__version__)))
+    detail = ''.join(('Image: ', sourcefilename, '\nX=', str(X), ' Y=', str(Y), ' Z=', str(Z), ' maxcolors=', str(maxcolors)))
+    showinfo(
+        title='Program information',
+        message=message,
+        detail=detail,
+    )
 
 
 def GetSource(event=None):
     """Opening source image and redefining other controls state"""
 
     global zoom_factor, zoom_do, zoom_show, preview, preview_data
-    global X, Y, Z, maxcolors, image3D
+    global X, Y, Z, maxcolors, image3D, sourcefilename
     zoom_factor = 0
+
     sourcefilename = filedialog.askopenfilename(title='Open image file', filetypes=[('Supported formats', '.png .ppm .pgm .pbm'), ('PNG', '.png'), ('PNM', '.ppm .pgm .pbm')])
     if sourcefilename == '':
         return
+
+    UIBusy()
 
     """ ┌────────────────────────────────────────┐
         │ Loading file, converting data to list. │
@@ -61,13 +93,10 @@ def GetSource(event=None):
     else:
         raise ValueError('Extension not recognized')
 
-    label_info.config(text='X=' + str(X) + ' Y=' + str(Y) + ' Z=' + str(Z) + ' maxcolors=' + str(maxcolors))
-    sortir.update()
-
     """ ┌─────────────────────────────────────────────────────────────────────────┐
         │ Converting list to bytes of PPM-like structure "preview_data" in memory │
         └────────────────────────────────────────────────────────────────────────-┘ """
-    preview_data = pnmlpnm.list2bin(image3D, maxcolors, show_chessboard = True)
+    preview_data = pnmlpnm.list2bin(image3D, maxcolors, show_chessboard=True)
 
     """ ┌────────────────────────────────────────────────┐
         │ Now showing "preview_data" bytes using Tkinter │
@@ -98,21 +127,24 @@ def GetSource(event=None):
     }
 
     preview = zoom_do[zoom_factor]
-    zanyato.config(text='Source', font=('helvetica', 8), image=preview, compound='top', foreground='grey')
+    zanyato.config(image=preview, compound='none', foreground='grey')
     # binding zoom on preview click
-    zanyato.bind('<Button-1>', zoomIn)  # left click
+    zanyato.bind('<Control-Button-1>', zoomIn)  # Ctrl + left click
+    zanyato.bind('<Double-Control-Button-1>', zoomIn)  # Ctrl + left click too fast
     zanyato.bind('<Alt-Button-1>', zoomOut)  # Alt + left click
-    zanyato.bind('<Button-2>', zoomOut)  # middle click
-    zanyato.bind('<Button-3>', zoomOut)  # right click
+    zanyato.bind('<Double-Alt-Button-1>', zoomOut)  # Alt + left click too fast
+    zanyato.bind('<MouseWheel>', zoomWheel)  # Wheel
     # enabling zoom buttons
     butt_plus.config(state='normal', cursor='hand2')
     butt_minus.config(state='normal', cursor='hand2')
     # updating zoom label display
     label_zoom.config(text=zoom_show[zoom_factor])
     # enabling "Save as..."
-    butt02.config(state='normal', cursor='hand2')
-    butt03.config(state='normal', cursor='hand2')
-    butt04.config(state='normal', cursor='hand2')
+    menu01.entryconfig('Save binary PNM...', state='normal')  # Instead of name numbers from 0 may be used
+    menu01.entryconfig('Save ascii PNM...', state='normal')
+    menu01.entryconfig('Save PNG...', state='normal')
+
+    UINormal()
 
 
 def SaveAsBin():
@@ -122,13 +154,15 @@ def SaveAsBin():
     if Z < 3:
         format = [('Portable grey map', '.pgm')]
         extension = ('Portable grey map', '.pgm')
+        filetype = 'PGM'
     else:
         format = [('Portable pixel map', '.ppm')]
         extension = ('Portable pixel map', '.ppm')
+        filetype = 'PPM'
 
     # Open "Save as..." file
     savefilename = filedialog.asksaveasfilename(
-        title='Save PNM file',
+        title='Save {ext} file'.format(ext=filetype),
         filetypes=format,
         defaultextension=extension,
     )
@@ -138,7 +172,9 @@ def SaveAsBin():
     """ ┌───────────────────────────────────────────────────────┐
         │ Converting list to bytes and saving as "savefilename" │
         └──────────────────────────────────────────────────────-┘ """
+    UIBusy()
     pnmlpnm.list2pnm(savefilename, image3D, maxcolors)
+    UINormal()
 
 
 def SaveAsAscii():
@@ -148,13 +184,15 @@ def SaveAsAscii():
     if Z < 3:
         format = [('Portable grey map', '.pgm')]
         extension = ('Portable grey map', '.pgm')
+        filetype = 'PGM'
     else:
         format = [('Portable pixel map', '.ppm')]
         extension = ('Portable pixel map', '.ppm')
+        filetype = 'PPM'
 
     # Open "Save as..." file
     savefilename = filedialog.asksaveasfilename(
-        title='Save PNM file',
+        title='Save {ext} file'.format(ext=filetype),
         filetypes=format,
         defaultextension=extension,
     )
@@ -164,7 +202,10 @@ def SaveAsAscii():
     """ ┌────────────────────────────────────────────────────────┐
         │ Converting list to string and saving as "savefilename" │
         └───────────────────────────────────────────────────────-┘ """
+    UIBusy()
     pnmlpnm.list2pnmascii(savefilename, image3D, maxcolors)
+    UINormal()
+
 
 def SaveAsPNG():
     """Once pressed on Save PNG"""
@@ -189,7 +230,9 @@ def SaveAsPNG():
     """ ┌───────────────────────────────────┐
         │ Feeding list to PyPNG via pnglpng │
         └──────────────────────────────────-┘ """
+    UIBusy()
     pnglpng.list2png(savefilename, image3D, info)
+    UINormal()
 
 
 def zoomIn(event=None):
@@ -197,7 +240,7 @@ def zoomIn(event=None):
     zoom_factor = min(zoom_factor + 1, 4)  # max zoom 5
     preview = PhotoImage(data=preview_data)
     preview = zoom_do[zoom_factor]
-    zanyato.config(image=preview, compound='top')
+    zanyato.config(image=preview, compound='none')
     # updating zoom factor display
     label_zoom.config(text=zoom_show[zoom_factor])
     # reenabling +/- buttons
@@ -213,7 +256,7 @@ def zoomOut(event=None):
     zoom_factor = max(zoom_factor - 1, -4)  # min zoom 1/5
     preview = PhotoImage(data=preview_data)
     preview = zoom_do[zoom_factor]
-    zanyato.config(image=preview, compound='top')
+    zanyato.config(image=preview, compound='none')
     # updating zoom factor display
     label_zoom.config(text=zoom_show[zoom_factor])
     # reenabling +/- buttons
@@ -224,52 +267,64 @@ def zoomOut(event=None):
         butt_minus.config(state='normal', cursor='hand2')
 
 
+def zoomWheel(event):
+    if event.delta < 0:
+        zoomOut()
+    if event.delta > 0:
+        zoomIn()
+
+
 """ ╔═══════════╗
     ║ Main body ║
     ╚═══════════╝ """
 
+# Starting values
+zoom_factor = 0
+X = Y = Z = maxcolors = None
+sourcefilename = 'None'
+
 sortir = Tk()
 
-zoom_factor = 0
-
-sortir.title('PNMViewer for Python 3.4')
+sortir.title('PNMViewer')
 sortir.geometry('+200+100')
-sortir.minsize(360, 240)
+# sortir.minsize(360, 240)
 
 # Main dialog icon is PPM as well!
-icon = PhotoImage(data=b'P6\n2 2\n255\n\xff\x00\x00\xff\xff\x00\x00\x00\xff\x00\xff\x00')
-sortir.iconphoto(True, icon)
+sortir.iconphoto(True, PhotoImage(data=b'P6\n2 2\n255\n\xff\x00\x00\xff\xff\x00\x00\x00\xff\x00\xff\x00'))
 
-label_info = Label(sortir, text='PNMViewer ' + str(pnmlpnm.__name__) + ' v. ' + str(pnmlpnm.__version__), font=('courier', 8), foreground='grey')
-label_info.pack(side='bottom', padx=0, pady=1, fill='both')
+menu01 = Menu(sortir, tearoff=False)  # Main menu, currently one "File" entry
 
-frame_left = Frame(sortir, borderwidth=2, relief='groove')
-frame_left.pack(side='left', anchor='nw')
-frame_right = Frame(sortir, borderwidth=2, relief='groove')
-frame_right.pack(side='left', anchor='nw')
+menu01.add_command(label='Open...', state='normal', command=GetSource)
+menu01.add_command(label='Save binary PNM...', state='disabled', command=SaveAsBin)
+menu01.add_command(label='Save ascii PNM...', state='disabled', command=SaveAsAscii)
+menu01.add_command(label='Save PNG...', state='disabled', command=SaveAsPNG)
+menu01.add_separator()
+menu01.add_command(label='Info', command=ShowInfo)
+menu01.add_separator()
+menu01.add_command(label='Exit', state='normal', command=DisMiss)
 
-butt01 = Button(frame_left, text='Open image...'.center(24, ' '), font=('helvetica', 16), cursor='hand2', justify='center', command=GetSource)
-butt01.pack(side='top', padx=4, pady=[2, 12], fill='both')
+sortir.bind('<Button-3>', ShowMenu)
 
-butt02 = Button(frame_left, text='Save binary PPM/PGM...', font=('helvetica', 12), cursor='arrow', justify='center', state='disabled', command=SaveAsBin)
-butt02.pack(side='top', padx=4, pady=2, fill='both')
+frame_img = Frame(sortir, borderwidth=2, relief='groove')
+frame_img.pack(side='top')
 
-butt03 = Button(frame_left, text='Save ASCII PPM/PGM...', font=('helvetica', 12), cursor='arrow', justify='center', state='disabled', command=SaveAsAscii)
-butt03.pack(side='top', padx=4, pady=2, fill='both')
+zanyato = Label(
+    frame_img,
+    text='Preview area.\nDouble click to open,\nRight click for a menu.\nWhen opened,\nCtrl+Click to zoom in,\nAlt+Click to zoom out.',
+    font=('helvetica', 12),
+    justify='center',
+    borderwidth=2,
+    padx=24,
+    pady=24,
+    relief='groove',
+    foreground='dark blue',
+    background='light blue',
+    cursor='arrow',
+)
+zanyato.bind('<Double-Button-1>', GetSource)
+zanyato.pack(side='top', padx=0, pady=(0, 2))
 
-butt04 = Button(frame_left, text='Save PNG...', font=('helvetica', 12), cursor='arrow', justify='center', state='disabled', command=SaveAsPNG)
-butt04.pack(side='top', padx=4, pady=2, fill='both')
-
-butt99 = Button(frame_left, text='Exit', font=('helvetica', 16), cursor='hand2', justify='center', command=DisMiss)
-butt99.pack(side='bottom', padx=4, pady=[12, 2], fill='both')
-
-zanyato = Label(frame_right, text='Preview area'.center(16, ' '), font=('helvetica', 16), justify='center', borderwidth=2, relief='groove', foreground='brown', background='light grey')
-zanyato.bind('<Button-1>', GetSource)
-zanyato.bind('<Button-2>', GetSource)
-zanyato.bind('<Button-3>', GetSource)
-zanyato.pack(side='top')
-
-frame_zoom = Frame(frame_right, width=300, borderwidth=2, relief='groove')
+frame_zoom = Frame(frame_img, width=300, borderwidth=2, relief='groove')
 frame_zoom.pack(side='bottom')
 
 butt_plus = Button(frame_zoom, text='+', font=('courier', 8), width=2, cursor='arrow', justify='center', state='disabled', command=zoomIn)
