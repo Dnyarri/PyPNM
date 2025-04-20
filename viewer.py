dd92b48dd2a0ef443be500e1fa0dac535b7e3d02
Ilya Razmanov <ilyarazmanov@gmail.com>
2025-04-20 18:37:14 +0300

    GUI refinement

--- viewer.py
+++ viewer.py
@@ -3,15 +3,15 @@
 """Test shell for `PyPNM<https://github.com/Dnyarri/PyPNM/>`_ module - Tkinter-based viewer.
 
 Viewer does not use PPM file directly to display it with Tkinter PhotoImage(file=...) -
-instead, it loads file (in this case - PPM, PGM, or PBM, just because it's a demo for PyPNM module anyway),
-then constructs PPM-like bytes data object in memory, and then displays it using Tkinter PhotoImage(data=...).
+instead, it loads image file, then constructs PPM-like bytes data object in memory,
+and then displays it using Tkinter PhotoImage(data=...).
 For example, it's able to display ascii PGM and PPM, not directly supported by Tkinter,
 since it recodes them to binary on the fly.
 
 NOTE:
 
-This is special developer edition, including `PNG support<https://gitlab.com/drj11/pypng>`_,
-created deliberately to test LA and RGBA preview.
+This is special extended compatibility edition, tested with Python 3.4 under Windows XP,
+including `PNG support<https://gitlab.com/drj11/pypng>`_.
 
 """
 
@@ -19,7 +19,7 @@ __author__ = 'Ilya Razmanov'
 __copyright__ = '(c) 2025 Ilya Razmanov'
 __credits__ = 'Ilya Razmanov'
 __license__ = 'unlicense'
-__version__ = '2.16.18.34'
+__version__ = '2.16.20.34'
 __maintainer__ = 'Ilya Razmanov'
 __email__ = 'ilyarazmanov@gmail.com'
 __status__ = 'Production'
@@ -39,14 +39,13 @@ def DisMiss(event=None):
 
 def UINormal():
     """Normal UI state"""
-    zanyato.config(state='normal', cursor='arrow')
+    zanyato.config(state='normal')
     sortir.update()
 
 
 def UIBusy():
     """Busy UI state"""
-    zanyato.config(state='disabled', cursor='wait')
-    sortir.after(1, sortir.update())  # Otherwise cursor never updates
+    zanyato.config(state='disabled')
     sortir.update()
 
 
@@ -57,7 +56,24 @@ def ShowMenu(event):
 
 def ShowInfo():
     """Show program and module version"""
-    message = ''.join(('PNMViewer ver. ', str(__version__), '\nModules:\n', str(pnmlpnm.__name__), ' ver. ', str(pnmlpnm.__version__), '\n', str(pnglpng.__name__), ' ver. ', str(pnglpng.__version__), '\n', str(pnglpng.png.__name__), ' ver. ', str(pnglpng.png.__version__)))
+    message = ''.join(
+        (
+            'PNMViewer ver. ',
+            str(__version__),
+            '\nModules:\n',
+            str(pnmlpnm.__name__),
+            ' ver. ',
+            str(pnmlpnm.__version__),
+            '\n',
+            str(pnglpng.__name__),
+            ' ver. ',
+            str(pnglpng.__version__),
+            '\n',
+            str(pnglpng.png.__name__),
+            ' ver. ',
+            str(pnglpng.png.__version__),
+        )
+    )
     detail = ''.join(('Image: ', sourcefilename, '\nX=', str(X), ' Y=', str(Y), ' Z=', str(Z), ' maxcolors=', str(maxcolors)))
     showinfo(
         title='General information',
@@ -82,7 +98,7 @@ def GetSource(event=None):
     """ ┌────────────────────────────────────────┐
         │ Loading file, converting data to list. │
         │  NOTE: maxcolors, image3D are GLOBALS! │
-        │  This is required for preview to work. │
+        │  They are used during save!            │
         └────────────────────────────────────────┘ """
 
     if Path(sourcefilename).suffix == '.png':
@@ -130,13 +146,13 @@ def GetSource(event=None):
     }
 
     preview = zoom_do[zoom_factor]
-    zanyato.config(image=preview, compound='none', foreground='grey')
+    zanyato.config(image=preview, compound='none', borderwidth=1, background=zanyato.master['background'])
     # binding zoom on preview click
     zanyato.bind('<Control-Button-1>', zoomIn)  # Ctrl + left click
     zanyato.bind('<Double-Control-Button-1>', zoomIn)  # Ctrl + left click too fast
     zanyato.bind('<Alt-Button-1>', zoomOut)  # Alt + left click
     zanyato.bind('<Double-Alt-Button-1>', zoomOut)  # Alt + left click too fast
-    zanyato.bind('<MouseWheel>', zoomWheel)  # Wheel
+    sortir.bind_all('<MouseWheel>', zoomWheel)  # Wheel
     # enabling zoom buttons
     butt_plus.config(state='normal', cursor='hand2')
     butt_minus.config(state='normal', cursor='hand2')
@@ -290,7 +306,7 @@ sortir = Tk()
 
 sortir.title('PNMViewer')
 sortir.geometry('+200+100')
-# sortir.minsize(360, 240)
+sortir.minsize(128, 128)
 
 # Main dialog icon is PPM as well!
 sortir.iconphoto(True, PhotoImage(data=b'P6\n2 2\n255\n\xff\x00\x00\xff\xff\x00\x00\x00\xff\x00\xff\x00'))
@@ -317,7 +333,7 @@ frame_img.pack(side='top')
 
 zanyato = Label(
     frame_img,
-    text='Preview area.\n  Double click to open,\n  Right click or Alt+F for a menu.\nWhen opened,\n  Ctrl+Click to zoom in,\n  Alt+Click to zoom out.',
+    text='Preview area.\n  Double click to open image,\n  Right click or Alt+F for a menu.\nWith image opened,\n  Ctrl+Click to zoom in,\n  Alt+Click to zoom out.',
     font=('helvetica', 12),
     justify='left',
     borderwidth=2,
@@ -334,10 +350,10 @@ zanyato.pack(side='top', padx=0, pady=(0, 2))
 frame_zoom = Frame(frame_img, width=300, borderwidth=2, relief='groove')
 frame_zoom.pack(side='bottom')
 
-butt_plus = Button(frame_zoom, text='+', font=('courier', 8), width=2, cursor='arrow', justify='center', state='disabled', command=zoomIn)
+butt_plus = Button(frame_zoom, text='+', font=('courier', 8), width=2, cursor='arrow', justify='center', state='disabled', borderwidth=1, command=zoomIn)
 butt_plus.pack(side='left', padx=0, pady=0, fill='both')
 
-butt_minus = Button(frame_zoom, text='-', font=('courier', 8), width=2, cursor='arrow', justify='center', state='disabled', command=zoomOut)
+butt_minus = Button(frame_zoom, text='-', font=('courier', 8), width=2, cursor='arrow', justify='center', state='disabled', borderwidth=1, command=zoomOut)
 butt_minus.pack(side='right', padx=0, pady=0, fill='both')
 
 label_zoom = Label(frame_zoom, text='Zoom 1:1', font=('courier', 8), state='disabled')
