6c0a706a80e2fcdf2f231da75f094baca5710026
Ilya Razmanov <ilyarazmanov@gmail.com>
2025-04-20 18:41:14 +0300

    GUI refinement

--- viewer.py
+++ viewer.py
@@ -19,7 +19,7 @@ __author__ = 'Ilya Razmanov'
 __copyright__ = '(c) 2025 Ilya Razmanov'
 __credits__ = 'Ilya Razmanov'
 __license__ = 'unlicense'
-__version__ = '2.16.18.20'
+__version__ = '2.16.20.20'
 __maintainer__ = 'Ilya Razmanov'
 __email__ = 'ilyarazmanov@gmail.com'
 __status__ = 'Production'
@@ -39,14 +39,13 @@ def DisMiss(event=None):
 
 def UINormal():
     """Normal UI state"""
-    zanyato.config(state='normal', cursor='arrow')
+    zanyato.config(state='normal')
     sortir.update()
 
 
 def UIBusy():
     """Busy UI state"""
-    zanyato.config(state='disabled', cursor='wait')
-    sortir.after(1, sortir.update())  # Otherwise cursor never updates
+    zanyato.config(state='disabled')
     sortir.update()
 
 
@@ -80,7 +79,7 @@ def GetSource(event=None):
     """ ┌────────────────────────────────────────┐
         │ Loading file, converting data to list. │
         │  NOTE: maxcolors, image3D are GLOBALS! │
-        │  This is required for preview to work. │
+        │  They are used during save!            │
         └────────────────────────────────────────┘ """
 
     if Path(sourcefilename).suffix == '.png':
@@ -128,13 +127,13 @@ def GetSource(event=None):
     }
 
     preview = zoom_do[zoom_factor]
-    zanyato.config(image=preview, compound='none', foreground='grey')
+    zanyato.config(image=preview, compound='none', borderwidth=1, background=zanyato.master['background'])
     # binding zoom on preview click
     zanyato.bind('<Control-Button-1>', zoomIn)  # Ctrl + left click
     zanyato.bind('<Double-Control-Button-1>', zoomIn)  # Ctrl + left click too fast
     zanyato.bind('<Alt-Button-1>', zoomOut)  # Alt + left click
     zanyato.bind('<Double-Alt-Button-1>', zoomOut)  # Alt + left click too fast
-    zanyato.bind('<MouseWheel>', zoomWheel)  # Wheel
+    sortir.bind_all('<MouseWheel>', zoomWheel)  # Wheel
     # enabling zoom buttons
     butt_plus.config(state='normal', cursor='hand2')
     butt_minus.config(state='normal', cursor='hand2')
@@ -287,7 +286,7 @@ sortir = Tk()
 
 sortir.title('PNMViewer')
 sortir.geometry('+200+100')
-# sortir.minsize(360, 240)
+sortir.minsize(128, 128)
 
 # Main dialog icon is PPM as well!
 sortir.iconphoto(True, PhotoImage(data=b'P6\n2 2\n255\n\xff\x00\x00\xff\xff\x00\x00\x00\xff\x00\xff\x00'))
@@ -314,7 +313,7 @@ frame_img.pack(side='top')
 
 zanyato = Label(
     frame_img,
-    text='Preview area.\n  Double click to open,\n  Right click or Alt+F for a menu.\nWhen opened,\n  Ctrl+Click to zoom in,\n  Alt+Click to zoom out.',
+    text='Preview area.\n  Double click to open image,\n  Right click or Alt+F for a menu.\nWith image opened,\n  Ctrl+Click to zoom in,\n  Alt+Click to zoom out.',
     font=('helvetica', 12),
     justify='left',
     borderwidth=2,
@@ -331,10 +330,10 @@ zanyato.pack(side='top', padx=0, pady=(0, 2))
 frame_zoom = Frame(frame_img, width=300, borderwidth=2, relief='groove')
 frame_zoom.pack(side='bottom')
 
-butt_plus = Button(frame_zoom, text='+', font=('courier', 8), width=2, cursor='arrow', justify='center', state='disabled', command=zoomIn)
+butt_plus = Button(frame_zoom, text='+', font=('courier', 8), width=2, cursor='arrow', justify='center', state='disabled', borderwidth=1, command=zoomIn)
 butt_plus.pack(side='left', padx=0, pady=0, fill='both')
 
-butt_minus = Button(frame_zoom, text='-', font=('courier', 8), width=2, cursor='arrow', justify='center', state='disabled', command=zoomOut)
+butt_minus = Button(frame_zoom, text='-', font=('courier', 8), width=2, cursor='arrow', justify='center', state='disabled', borderwidth=1, command=zoomOut)
 butt_minus.pack(side='right', padx=0, pady=0, fill='both')
 
 label_zoom = Label(frame_zoom, text='Zoom 1:1', font=('courier', 8), state='disabled')
