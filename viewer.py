c13eec30cb9e2fbc4d2962cf3bd18c80da6db3ed
Ilya Razmanov <ilyarazmanov@gmail.com>
2025-11-12 14:39:58 +0300

    PyPNM 2.23.13.13

--- viewer.py
+++ viewer.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-"""Test shell for `PyPNM<https://github.com/Dnyarri/PyPNM/>`_ module - Tkinter-based viewer.
+"""Test shell for `PyPNM for Python >= 3.11`_ module - a Tkinter-based viewer.
 
-Viewer does not use PPM file directly to display it with Tkinter PhotoImage(file=...) -
-instead, it loads image file (in this case - PPM, PGM, or PBM, just because it's a demo
-for PyPNM module anyway), then constructs PPM-like bytes data object in memory, and then
-displays it using Tkinter PhotoImage(data=...).
-For example, it's able to display ascii PGM and PPM, not directly supported by Tkinter,
+Viewer does not use PNM file directly to display it with Tkinter ``PhotoImage(file=...)`` -
+instead, it loads image file, then constructs PNM-like bytes data object in memory,
+and then displays it using Tkinter ``PhotoImage(data=...)``.
+For example, it's able to display ASCII PGM and PPM, not supported by Tkinter,
 since it recodes them to binary on the fly.
+
+.. _PyPNM for Python >= 3.11: https://github.com/Dnyarri/PyPNM
 
 """
 
@@ -15,7 +16,7 @@ __author__ = 'Ilya Razmanov'
 __copyright__ = '(c) 2025 Ilya Razmanov'
 __credits__ = 'Ilya Razmanov'
 __license__ = 'unlicense'
-__version__ = '2.23.7.23'
+__version__ = '2.23.13.13'
 __maintainer__ = 'Ilya Razmanov'
 __email__ = 'ilyarazmanov@gmail.com'
 __status__ = 'Production'
